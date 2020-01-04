321f56ec23a8d6a8c702a19ca310e5d98015f9c7
Nick Anthony <Ilithium@protonmail.com>
2020-01-05 00:19:36 +0100

    Complete performance

--- main/performance/performance.go
+++ main/performance/performance.go
@@ -3,18 +3,40 @@ package performance
 import (
 	"database/sql"
 	"fmt"
-	"log"
-	"time"
 
 	"github.com/Liberatys/libra-back/main/user"
 )
 
+//Performance entry for performance
+type Performance struct {
+	Performance int64  `json:"performance"`
+	Date        string `json:"date"`
+}
+
+//LoadUserPerformance loads the performance of a single user
+func LoadUserPerformance(userID int64, connection *sql.DB) []Performance {
+	rows, err := connection.Query("SELECT performance,date from performance WHERE userid = ?")
+	defer rows.Close()
+	if err != nil {
+		return nil
+	}
+	var performances []Performance
+	for rows.Next() {
+		var performance Performance
+		err := rows.Scan(&performance.Performance, &performance.Date)
+		if err != nil {
+			fmt.Println("Fatal error getting ids from rows")
+		}
+		performances = append(performances, performance)
+	}
+	return performances
+}
+
 //UpdatePerformance updates the current perfomance of all user portfolios -> performance.sql
 func UpdatePerformance(connection *sql.DB) {
 	var userIDs = getUsers(connection)
-	fmt.Println("%v", userIDs)
-	for _, id := range userIDs {
-		var userportfolio = user.LoadPortfolio(id, connection)
+	for index := range userIDs {
+		var userportfolio = user.LoadPortfolio(userIDs[index], connection)
 		var currentBalance, _ = userportfolio.Balance.Float64()
 		//fmt.Println(currentBalance)
 		var currentValue, _ = userportfolio.CurrentValue.Float64()
@@ -23,10 +45,8 @@ func UpdatePerformance(connection *sql.DB) {
 		//fmt.Println(startCapital)
 		var result = (currentBalance + currentValue) - startCapital
 		//fmt.Println(result)
-
 		var performance = result / startCapital
-		writePerformance(id, performance, connection)
-		fmt.Println(performance)
+		writePerformance(userIDs[index], performance, connection)
 	}
 }
 
@@ -38,7 +58,6 @@ func getUsers(connection *sql.DB) []int64 {
 		return nil
 	}
 	defer rows.Close()
-
 	var userIDs []int64
 	for rows.Next() {
 		var id int64
@@ -54,20 +73,13 @@ func getUsers(connection *sql.DB) []int64 {
 
 // writes the calculated performance into the database
 func writePerformance(userID int64, performance float64, connection *sql.DB) {
-	currentTime := time.Now()
-	sqlQuery := "INSERT INTO `performance`(userid`, `date`, `performance`) VALUES (?,?,?)"
+	sqlQuery := "INSERT INTO performance(userid, date, performance) VALUES (?,CURDATE(),?)"
 	statement, err := connection.Prepare(sqlQuery)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
 	}
-
-	res, err := statement.Exec(userID, currentTime.Format("1999-01-01"), performance)
+	_, err = statement.Exec(userID, performance)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
 	}
-	rowCnt, err := res.RowsAffected()
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("affected = %d\n", rowCnt)
 }
